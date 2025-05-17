7eeabdb06b2928ecac816dabafd15d6247ab0cd3
Vladimir Solo <freeman.wushu@mail.ru>
2025-05-17 19:48:06 +0300

    feat: add solution contract basic bank v2

--- BasicBankV2/src/BasicBankV2.sol
+++ BasicBankV2/src/BasicBankV2.sol
@@ -2,15 +2,35 @@
 pragma solidity ^0.8.13;
 
 contract BasicBankV2 {
-    /// used to store the balance of users
-    ///     USER    =>  BALANCE
+    /// @notice Tracks deposited ETH per user
     mapping(address => uint256) public balances;
 
-    /// @notice deposit ether into the contract
-    /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    /// @notice Emitted on successful deposit
+    event Deposited(address indexed user, uint256 amount);
 
-    /// @notice used to withdraw ether from the contract
-    /// @param amount of ether to remove. Cannot execeed balance i.e users cannot withdraw more than they deposited
-    function removeEther(uint256 amount) external payable {}
+    /// @notice Emitted on successful withdrawal
+    event Withdrawn(address indexed user, uint256 amount);
+
+    /// @notice Deposit ETH and update user balance
+    /// @dev Requires non-zero ETH value
+    function addEther() external payable {
+        require(msg.value > 0, "Zero deposit not allowed");
+        balances[msg.sender] += msg.value;
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    /// @notice Withdraw specified ETH amount
+    /// @dev Implements checks-effects-interactions pattern
+    function removeEther(uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Insufficient user balance");
+
+        // Update balance before external call
+        balances[msg.sender] -= amount;
+
+        // Secure ETH transfer
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "ETH transfer reverted");
+
+        emit Withdrawn(msg.sender, amount);
+    }
 }
